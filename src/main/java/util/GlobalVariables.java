e8ac91422e2c7cae5606aa419976580660ba225c
Janis Smilga <janissmilga@Janiss-MacBook-Pro-4.local>
2026-04-15 20:09:57 +0300

    timer

--- src/main/java/util/GlobalVariables.java
+++ src/main/java/util/GlobalVariables.java
@@ -5,5 +5,5 @@ import java.time.Duration;
 public class GlobalVariables {
 
     public static final String appiumLocalUrl = "http://127.0.0.1:4723/";
-    public static final Duration globalTimeout = Duration.ofSeconds(5);
+    public static final Duration globalTimeout = Duration.ofSeconds(10);
 }
